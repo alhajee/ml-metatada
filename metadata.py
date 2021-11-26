281cd114230b9a28e22aaf3c552f14887796577d
Muhammad Dauda <alhajee2009@gmail.com>
2021-11-26 11:35:42 +0100

    save metadata from argument

--- metadata.py
+++ metadata.py
@@ -101,7 +101,7 @@ class Metadata:
 
 
     def _save_metadata(self, metadata, path: str) -> None:
-        joblib.dump(self.metadata, path)
+        joblib.dump(metadata, path)
 
 
     def _load_metadata(self, path: str) -> None:
